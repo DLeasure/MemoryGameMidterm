6bcaec8c75664723e25697ed6786389053ed73b0
jon plumb <jplumb89@outlook.com>
2019-06-13 18:41:59 -0400

    card flip/match solution

--- MemoryGame.js
+++ MemoryGame.js
@@ -1,40 +1,61 @@
+const cards = document.querySelectorAll('.memory-card');
 
-function randomizeCards() {
-    let pokemon = ["Squirtle", "Squirtle", "Scyther", "Scyther", "Growlithe", "Growlithe", "Mewtwo", "Mewtwo", "Onix", "Onix", "Bulbasaur", "Bulbasaur"];
-    for (i = 1; i <= 12; i++) {
-        let pokemonCard = document.getElementById(`${i}`);
-        const pokemonArrayNum = Math.floor(Math.random() * (pokemon.length))
-        const pokemonForThisCard = pokemon.splice(pokemonArrayNum, 1);
-        switch(pokemonForThisCard[0]) {
-            case "Squirtle":
-                pokemonCard.style.background = "url('squirtle.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-            case "Scyther":
-                pokemonCard.style.background = "url('scyther.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-            case "Growlithe":
-                pokemonCard.style.background = "url('growlithe.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-            case "Mewtwo":
-                pokemonCard.style.background = "url('mewtwo.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-            case "Onix":
-                pokemonCard.style.background = "url('onix.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-            case "Bulbasaur":
-                pokemonCard.style.background = "url('bulbasaur.png')";
-                pokemonCard.style.backgroundSize = "150px 200px";
-                break;
-        }
-    }
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
 }
 
-randomizeCards();
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+  isMatch ? disableCards() : unflipCards();
+}
 
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
+
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
