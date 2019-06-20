58364695c2cf18bb4976f57b0294bcdefc8e2f56
Ryan Morris <rmorris00@gmail.com>
2019-06-20 18:34:58 -0400

    hopefullythisworks

--- MemoryGame.js
+++ MemoryGame.js
@@ -1,89 +1,60 @@
-function randomizeCards() {
-    let pokemon = ["Squirtle", "Squirtle", "Scyther", "Scyther", "Growlithe", "Growlithe", "Mewtwo", "Mewtwo", "Onix", "Onix", "Bulbasaur", "Bulbasaur"];
-    for (i = 1; i <= 12; i++) {
-        let pokemonCard = document.getElementById(`${i}`);
-        const pokemonArrayNum = Math.floor(Math.random() * (pokemon.length))
-        const pokemonForThisCard = pokemon.splice(pokemonArrayNum, 1);
-        pokemonCard.className += " " + pokemonForThisCard;
-        console.log(pokemonCard.className);
-    }
-}
+let wonGame = false;
 
-// randomizeCards();
+
+
+
 
 function startCountdown(seconds) {
-    let counter = seconds;
+    counter = seconds;
 
     var interval = setInterval(() => {
-
         counter--;
-        document.getElementById('timer').innerHTML = "0:" + seconds--;
+        document.getElementById("timer").innerHTML = "0:" + seconds--;
 
+        if (wonGame === true) {
+            document.getElementById("timer").innerHTML = "You won!";
+            clearInterval(interval);
+            return;
+        }
         if (counter >= 10) {
             document.getElementById("timer").innerHTML = "0:" + counter;
         }
-
         if (counter <= 9 && counter > 0) {
             document.getElementById("timer").innerHTML = "0:0" + counter;
-        };
+        }
         if (counter === 0) {
-
-            clearInterval(interval);
             document.getElementById("timer").innerHTML = "GAME OVER!";
-
+            alert("Sorry, you lost!");
+            clearInterval(interval);
+            // break;
         };
     }, 1000);
 };
 
-
-// document.getElementById("timer").addEventListener("onclick", startCountdown(29));
-
-if (document.getElementById("timer") === "GAME OVER!") {
-    alert("Sorry, you lost!");
-};
-
-/*
-
-FUNCTION FROM ADDRESS BOOK LAB THAT WILL GRAB WINNER 
-function display() {
-
-    let x = document.createElement("ul");
-
-    x.setAttribute("class", "contact-list");
-
-    let t = document.createElement("li");
-
-    let t1 = document.createTextNode("Name: " + document.getElementById('name').value);
-
-    t.appendChild(t1);
-
-   
-
-
-
-    x.appendChild(t);};}
-    */
-
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+const resetButton = document.querySelector("#reset");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+resetButton.addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+// Get the play element that closes the modal
+let playButton = document.querySelector("#close");
 
 // When the user clicks on the button, open the modal 
 window.onload = function modalLoad() {
-    modal.style.display = "block";
-}
+    winModal.style.display = "none";
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+playButton.addEventListener("click", function() {
     modal.style.display = "none";
     startCountdown(30);
     startGame();
-}
+});
 
 
 // When the user clicks anywhere outside of the modal, close it
@@ -93,77 +64,158 @@ window.onclick = function(event) {
     }
 }
 
-function openModal() {
-    $(document).ready(function() {
-        $("#myModal").modal();
-    });
+///////////////////////////////////////////////////// WINmodal
+const winModal = document.getElementById("winModal");
+const winMessage = document.getElementById("winMessage");
+const winInput = document.getElementById("winInput");
+// const winOk = document.getElementById("winOk");
+// winOk.addEventListener("click", function() {
+//     addName();
+//     winModal.style.display = "none";
+// });
+// winOk.addEventListener("click", );
+
+
+
+
+
+
+// When the user clicks on the button, open the modal 
+// window.onload = function modalLoad() {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// winOk.addEventListener("click", function() {
+//     modal.style.display = "none";
+
+// });
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        winModal.style.display = "none";
+    }
 }
 
-function closeModal() {
-    $(document).ready(function() {
-        $("#myModal").modal('hide');
-    });
-}
+
+
+
+
+
 
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
 
-function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+function startGame() {
+    wonGame = false;
+    let pokemonCardsMatched = 0;
+    const cards = document.querySelectorAll('.memory-card');
 
-    this.classList.add('flip');
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
+    function flipCard() {
+        if (lockBoard) return;
+        if (this === firstCard) return;
 
-        return;
+        this.classList.add('flip');
+
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+        }
+
+        secondCard = this;
+        checkForMatch();
     }
 
-    secondCard = this;
-    checkForMatch();
-}
+    function checkForMatch() {
+        let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+        isMatch ? disableCards() : unflipCards();
+    }
 
-    isMatch ? disableCards() : unflipCards();
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+        pokemonCardsMatched += 2;
+        if (pokemonCardsMatched < 12) {
+            resetBoard();
+        } else if (pokemonCardsMatched === 12) {
+            wonGame = true;
 
-}
+            const winTime = 30 - counter;
 
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
 
-    resetBoard();
+            function addName() {
 
-}
+                // var winner = document.createElement("LI");
+                // var d = document.createElement("li");
+                // winner.classList.add(winInput);
+                // document.getElementById("winInput").appendChild(d);
 
-function unflipCards() {
-    lockBoard = true;
 
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
 
-        resetBoard();
-    }, 1500);
-}
+                // winner.appendChild(d);
+                // document.getElementById("leaders").appendChild(winner);
 
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-}
+                let x = winInput.value;
+                var node = document.createElement("LI");
+                var textnode = document.createTextNode(x + " " + winTime);
+                node.appendChild(textnode);
+                document.getElementById("leaders").appendChild(node);
+                return;
 
-(function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
-        card.style.order = randomPos;
-    });
-})();
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+            };
+            winModal.style.display = "block";
+            winMessage.innerHTML = 'You won the game in ' + winTime + ' seconds!';
+
+
+            winOk.addEventListener("click", function() {
+                addName();
+                winModal.style.display = "none";
+                return;
+            });
+
+
+
+
+            document.querySelector(".leaderboard").style.display = "inline";
+            resetBoard();
+            return;
+        }
+
+    }
+
+    function unflipCards() {
+        lockBoard = true;
+
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+
+            resetBoard();
+        }, 700);
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    cards.forEach(card => card.classList.remove('flip'));
+
+    (function shuffle() {
+        cards.forEach(card => {
+            let randomPos = Math.floor(Math.random() * 12);
+            card.style.order = randomPos;
+        });
+    })();
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
+};
